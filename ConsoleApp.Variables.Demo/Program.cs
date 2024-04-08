651beefef1133f6f659c156548db0d095abfeac6
rmalk <rmalk@THEMACHINE>
2024-04-08 04:03:36 -0400

    Add project files.

---
+++ ConsoleApp.Variables.Demo/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+// declare variables
+string name = string.Empty;
+int age = 0;
+int retirementAge = 67;
+
+Console.WriteLine("Enter your name:");
+name = Console.ReadLine();
+Console.WriteLine("Enter your current age:");
+age = Convert.ToInt32(Console.ReadLine());
+int workingYearsRemaining = retirementAge - age;
+
+Console.WriteLine($"Employee's name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Years to retirement: {workingYearsRemaining}");
+
+
