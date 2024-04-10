91371cf892861fe70da9b0056e07ff02327d523b
rckmlk <rmalkemes@gmail.com>
2024-04-10 03:12:21 -0400

    update

---
+++ ConsoleApp.ConditionsAndDecisions/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter student's grade(0 to 100)");
+int grade = Convert.ToInt32(Console.ReadLine());
+
+if (grade > 80)
+{
+    Console.WriteLine("Student passed");
+}
+
+else
+{
+    Console.WriteLine("Student failed!");
+}
+
+
+
--- ConsoleApp.Variables.Demo/Program.cs
+++ ConsoleApp.Variables.Demo/Program.cs
@@ -1,11 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // declare variables
-string name = string.Empty;
+string firstName = string.Empty;
+string lastName = string.Empty;
 int age = 0;
 int retirementAge = 70;
+decimal salary = 0;
+char gender = char.MinValue;
+bool working = false;
 
-Console.WriteLine("Enter your name:");
-name = Console.ReadLine();
+
+
+Console.WriteLine("Enter your first name:");
+firstName = Console.ReadLine();
+
+Console.WriteLine("Enter your last name:");
+lastName = Console.ReadLine();
+
+
 Console.WriteLine("Enter your current age:");
 age = Convert.ToInt32(Console.ReadLine());
 int workingYearsRemaining = retirementAge - age;
