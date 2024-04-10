d1e5e2efb90d2941b18facd84656f9f852c5d7a3
rckmlk <rmalkemes@gmail.com>
2024-04-10 03:46:49 -0400

    more adds

--- ConsoleApp.ConditionsAndDecisions/Program.cs
+++ ConsoleApp.ConditionsAndDecisions/Program.cs
@@ -2,15 +2,40 @@
 Console.Write("Enter student's grade(0 to 100)");
 int grade = Convert.ToInt32(Console.ReadLine());
 
-if (grade > 80)
-{
-    Console.WriteLine("Student passed");
-}
+/*
+ * A 86 to 100
+ * B 85 to 75
+ * c 65 to 74
+ * d 50 to 74
+ * f less than 50
+ */
 
+if ((grade >= 0) && (grade <= 100))
+{
+
+    if (grade > 85)
+    {
+        Console.WriteLine("Student's grade is A! ");
+    }
+    else if (grade > 74)
+    {
+        Console.WriteLine("Student's grade is B! ");
+    }
+    else if (grade > 64)
+    {
+        Console.WriteLine("Student's grade is C! ");
+    }
+    else if (grade > 49)
+    {
+        Console.WriteLine("Student's grade is D! ");
+    }
+    else
+    {
+        Console.WriteLine("Student has failed! ");
+    }
+}
 else
 {
-    Console.WriteLine("Student failed!");
+    Console.WriteLine("Enter valid grade from 0 to 100: 1");
 }
-
-
 
